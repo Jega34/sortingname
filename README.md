e7616b3899e5fd5407c9739a68587e7d6f7137f3
Jega34 <jegathesh.a@anjaconline.org>
2021-11-24 19:37:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-xdmf3s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-xdmf3s)
---
+++ index.ts
@@ -0,0 +1,7 @@
+let values = ['Jega', 'Vijay', 'Praveen', 'Dhoni', 'Sam'];
+
+console.log(values.sort());
+
+console.log(values.reverse());
+
+console.log(values.concat());
